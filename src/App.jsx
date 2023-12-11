6581614563dc68b40bb9eefeff412d08060c9d2e
Vinayak Kumar <v27u27@gmail.com>
2023-12-11 13:18:53 +0530

    zwiggy setup init commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import zwiggyLogo from '/png-zwiggy-logo.png'
+import Header from "./components/Header.jsx";
+
+const App = () =>  {
+    return (
+        <Header/>
+        // <Body/>
+        // <Footer/>
+    );
+}
+
+export default App
