3a3eb2d7a0939242ab4fd47a8db1f4f5a8619370
Vinayak K <v27u27@gmail.com>
2023-12-29 17:36:24 +0530

    updated default restaurant list

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,6 +5,671 @@ export const restaurantMenuFetchApiUrl =
 export const restaurantImageCDNUrl =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 export const defaultData = [
+{
+"info": {
+"id": "5925",
+"name": "Burger King",
+"cloudinaryImageId": "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+"locality": "Ambience Mall",
+"areaName": "Vasant Kunj",
+"costForTwo": "₹350 for two",
+"cuisines": [
+"Burgers",
+"American"
+],
+"avgRating": 4.2,
+"parentId": "166",
+"avgRatingString": "4.2",
+"totalRatingsString": "10K+",
+"sla": {
+"deliveryTime": 27,
+"lastMileTravel": 2.6,
+"serviceability": "SERVICEABLE",
+"slaString": "27 mins",
+"lastMileTravelString": "2.6 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2023-12-30 01:00:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "60% OFF",
+"subHeader": "UPTO ₹120"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {}
+},
+"analytics": {
+"context": "seo-data-b2aa637d-67b8-4c74-83b2-c70e7e59c7f8"
+},
+"cta": {
+"link": "https://www.swiggy.com/restaurants/burger-king-ambience-mall-vasant-kunj-delhi-5925",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"info": {
+"id": "779575",
+"name": "Theobroma",
+"cloudinaryImageId": "16e2a6e4d4a137cf4649a7dbfd5e7e69",
+"locality": "Basant Lok",
+"areaName": "Vasant Vihar",
+"costForTwo": "₹400 for two",
+"cuisines": [
+"Bakery",
+"Desserts"
+],
+"avgRating": 4.6,
+"parentId": "1040",
+"avgRatingString": "4.6",
+"totalRatingsString": "100+",
+"sla": {
+"deliveryTime": 27,
+"lastMileTravel": 3.6,
+"serviceability": "SERVICEABLE",
+"slaString": "27 mins",
+"lastMileTravelString": "3.6 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2023-12-29 23:00:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "40% OFF",
+"subHeader": "ABOVE ₹179",
+"discountTag": "FLAT DEAL"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"isNewlyOnboarded": true,
+"restaurantOfferPresentationInfo": {}
+},
+"analytics": {
+"context": "seo-data-b2aa637d-67b8-4c74-83b2-c70e7e59c7f8"
+},
+"cta": {
+"link": "https://www.swiggy.com/restaurants/theobroma-basant-lok-vasant-vihar-delhi-779575",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"info": {
+"id": "53784",
+"name": "Pizza Hut",
+"cloudinaryImageId": "2b4f62d606d1b2bfba9ba9e5386fabb7",
+"locality": "Vasant Square Mall",
+"areaName": "Vasant Kunj",
+"costForTwo": "₹350 for two",
+"cuisines": [
+"Pizzas"
+],
+"avgRating": 3.9,
+"parentId": "721",
+"avgRatingString": "3.9",
+"totalRatingsString": "5K+",
+"sla": {
+"deliveryTime": 31,
+"lastMileTravel": 1,
+"serviceability": "SERVICEABLE",
+"slaString": "31 mins",
+"lastMileTravelString": "1.0 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2023-12-30 01:00:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "60% OFF",
+"subHeader": "UPTO ₹120"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {}
+},
+"analytics": {
+"context": "seo-data-b2aa637d-67b8-4c74-83b2-c70e7e59c7f8"
+},
+"cta": {
+"link": "https://www.swiggy.com/restaurants/pizza-hut-vasant-square-mall-vasant-kunj-delhi-53784",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"info": {
+"id": "104559",
+"name": "Starbucks Coffee",
+"cloudinaryImageId": "258fe8a3577877fbfe064095ed1d9dc3",
+"locality": "Ambience Mall",
+"areaName": "Vasant Kunj",
+"costForTwo": "₹400 for two",
+"cuisines": [
+"Beverages",
+"Cafe",
+"Snacks",
+"Desserts",
+"Bakery",
+"Ice Cream"
+],
+"avgRating": 4.1,
+"parentId": "195515",
+"avgRatingString": "4.1",
+"totalRatingsString": "100+",
+"sla": {
+"deliveryTime": 37,
+"lastMileTravel": 2.8,
+"serviceability": "SERVICEABLE",
+"slaString": "37 mins",
+"lastMileTravelString": "2.8 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2023-12-29 23:59:00",
+"opened": true
+},
+"badges": {
+"textExtendedBadges": [
+{
+"iconId": "guiltfree/GF_Logo_android_3x",
+"shortDescription": "options available",
+"fontColor": "#7E808C"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {
+"badgeObject": [
+{
+"attributes": {
+"description": "",
+"fontColor": "#7E808C",
+"iconId": "guiltfree/GF_Logo_android_3x",
+"shortDescription": "options available"
+}
+}
+]
+}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹219"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {}
+},
+"analytics": {
+"context": "seo-data-b2aa637d-67b8-4c74-83b2-c70e7e59c7f8"
+},
+"cta": {
+"link": "https://www.swiggy.com/restaurants/starbucks-coffee-ambience-mall-vasant-kunj-delhi-104559",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"info": {
+"id": "254000",
+"name": "McDonald's",
+"cloudinaryImageId": "6dc3ed2ca21d71acff7c2a51dfe4c720",
+"locality": "Ambience Mall",
+"areaName": "Vasant Kunj",
+"costForTwo": "₹400 for two",
+"cuisines": [
+"American"
+],
+"avgRating": 4.2,
+"parentId": "630",
+"avgRatingString": "4.2",
+"totalRatingsString": "5K+",
+"sla": {
+"deliveryTime": 37,
+"lastMileTravel": 2.6,
+"serviceability": "SERVICEABLE",
+"slaString": "37 mins",
+"lastMileTravelString": "2.6 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2023-12-29 23:30:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "10% OFF",
+"subHeader": "UPTO ₹40"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {}
+},
+"analytics": {
+"context": "seo-data-b2aa637d-67b8-4c74-83b2-c70e7e59c7f8"
+},
+"cta": {
+"link": "https://www.swiggy.com/restaurants/mcdonalds-ambience-mall-vasant-kunj-delhi-254000",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"info": {
+"id": "26717",
+"name": "KFC",
+"cloudinaryImageId": "f01666ac73626461d7455d9c24005cd4",
+"locality": "Vasant Square Mall",
+"areaName": "Vasant Kunj",
+"costForTwo": "₹400 for two",
+"cuisines": [
+"Burgers",
+"Biryani",
+"American",
+"Snacks",
+"Fast Food"
+],
+"avgRating": 4.1,
+"parentId": "547",
+"avgRatingString": "4.1",
+"totalRatingsString": "10K+",
+"sla": {
+"deliveryTime": 21,
+"lastMileTravel": 1.7,
+"serviceability": "SERVICEABLE",
+"slaString": "21 mins",
+"lastMileTravelString": "1.7 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2023-12-30 00:00:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "EVERY ITEM",
+"subHeader": "@ ₹179"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {}
+},
+"analytics": {
+"context": "seo-data-b2aa637d-67b8-4c74-83b2-c70e7e59c7f8"
+},
+"cta": {
+"link": "https://www.swiggy.com/restaurants/kfc-vasant-square-mall-vasant-kunj-delhi-26717",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"info": {
+"id": "25454",
+"name": "Domino's Pizza",
+"cloudinaryImageId": "qigvwjrzst9aw3izoanv",
+"locality": "Sector B",
+"areaName": "Vasant Kunj",
+"costForTwo": "₹400 for two",
+"cuisines": [
+"Pizzas",
+"Italian",
+"Pastas",
+"Desserts"
+],
+"avgRating": 4.2,
+"parentId": "2456",
+"avgRatingString": "4.2",
+"totalRatingsString": "10K+",
+"sla": {
+"deliveryTime": 25,
+"serviceability": "SERVICEABLE",
+"slaString": "25 mins",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2023-12-30 02:59:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "₹150 OFF",
+"subHeader": "ABOVE ₹299",
+"discountTag": "FLAT DEAL"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {}
+},
+"analytics": {
+"context": "seo-data-b2aa637d-67b8-4c74-83b2-c70e7e59c7f8"
+},
+"cta": {
+"link": "https://www.swiggy.com/restaurants/dominos-pizza-sector-b-vasant-kunj-delhi-25454",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"info": {
+"id": "524180",
+"name": "Bakingo",
+"cloudinaryImageId": "0c53fd8f62e3d25ba56d1b4cfe572cef",
+"locality": "Sector A",
+"areaName": "Vasant Kunj",
+"costForTwo": "₹299 for two",
+"cuisines": [
+"Bakery",
+"Desserts",
+"Beverages",
+"Snacks"
+],
+"avgRating": 4.4,
+"parentId": "3818",
+"avgRatingString": "4.4",
+"totalRatingsString": "1K+",
+"sla": {
+"deliveryTime": 29,
+"lastMileTravel": 3.8,
+"serviceability": "SERVICEABLE",
+"slaString": "29 mins",
+"lastMileTravelString": "3.8 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2023-12-30 01:00:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "₹175 OFF",
+"subHeader": "ABOVE ₹999",
+"discountTag": "FLAT DEAL"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {}
+},
+"analytics": {
+"context": "seo-data-b2aa637d-67b8-4c74-83b2-c70e7e59c7f8"
+},
+"cta": {
+"link": "https://www.swiggy.com/restaurants/bakingo-sector-a-vasant-kunj-delhi-524180",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"info": {
+"id": "680146",
+"name": "The Orange Twigs Cafe",
+"cloudinaryImageId": "8fa4b8394d1c5ef681f70a4247c4e021",
+"locality": "Jal Board Road",
+"areaName": "Vasant Kunj",
+"costForTwo": "₹600 for two",
+"cuisines": [
+"Italian",
+"American",
+"Salads",
+"Pastas",
+"Beverages",
+"Continental"
+],
+"avgRating": 4.2,
+"parentId": "212112",
+"avgRatingString": "4.2",
+"totalRatingsString": "100+",
+"sla": {
+"deliveryTime": 26,
+"lastMileTravel": 2.1,
+"serviceability": "SERVICEABLE",
+"slaString": "26 mins",
+"lastMileTravelString": "2.1 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2023-12-29 22:00:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "₹125 OFF",
+"subHeader": "ABOVE ₹249",
+"discountTag": "FLAT DEAL"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {}
+},
+"analytics": {
+"context": "seo-data-b2aa637d-67b8-4c74-83b2-c70e7e59c7f8"
+},
+"cta": {
+"link": "https://www.swiggy.com/restaurants/the-orange-twigs-cafe-jal-board-road-vasant-kunj-delhi-680146",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
   {
     info: {
       id: "622044",
